2647c68be9d8f1237a05e1759a24f177e433338f
Douglas Araújo <araujodgdev@gmail.com>
2026-06-01 18:30:37 -0300

    Bootstrap Backend com SpringBoot e Config do DB com docker compose

---
+++ sulwork-cafe/src/main/java/br/com/dgdev/sulwork/cafe/SulworkCafeApplication.java
@@ -0,0 +1,13 @@
+package br.com.dgdev.sulwork.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SulworkCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SulworkCafeApplication.class, args);
+	}
+
+}
---
+++ sulwork-cafe/src/test/java/br/com/dgdev/sulwork_cafe/SulworkCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dgdev.sulwork_cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SulworkCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
