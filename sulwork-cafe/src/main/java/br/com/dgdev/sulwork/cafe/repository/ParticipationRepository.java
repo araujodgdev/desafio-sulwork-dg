f6ff2f862a82da88c7889e9ec6e209533966b44a
Douglas Araújo <araujodgdev@gmail.com>
2026-06-02 15:15:31 -0300

    feat: create ParticipationRepository

---
+++ sulwork-cafe/src/main/java/br/com/dgdev/sulwork/cafe/repository/ParticipationRepository.java
@@ -0,0 +1,68 @@
+package br.com.dgdev.sulwork.cafe.repository;
+
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+
+@Repository
+public class ParticipationRepository {
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	
+	
+	public boolean collaboratorAlreadyExists(String cpf) {
+		String sql = """
+				SELECT COUNT(*)
+				FROM collaborators
+				WHERE cpf = :cpf
+				""";
+		
+		Query query = entityManager.createNativeQuery(sql);
+		query.setParameter("cpf", cpf);
+		
+		Number result = (Number) query.getSingleResult();
+		
+		return result.longValue() > 0;
+		
+	};
+	
+	public boolean itemAlreadyExistsByDate(String itemName, LocalDate breakfastDate) {
+		String sql = """
+				SELECT COUNT(*)
+				FROM breakfast_items
+				WHERE LOWER(itemName) = LOWER(:itemName)
+				AND breakfast_date = :breakfastDate
+				""";
+		
+		Query query = entityManager.createNativeQuery(sql);
+		query.setParameter("breakfastDate", breakfastDate);
+		query.setParameter("itemName", itemName);
+		
+		Number result = (Number) query.getSingleResult();
+		
+		return result.longValue() > 0;
+	};
+	
+	public Long insertNewCollaborator(String collaboratorName, String collaboratorCPF) {
+		String sql = """
+				INSERT INTO collaborators (name, cpf)
+				VALUES (:name, :cpf)
+				RETURNING id
+				""";
+		
+		Query query = entityManager.createNativeQuery(sql);
+		query.setParameter("name", collaboratorName);
+		query.setParameter("cpf", collaboratorCPF);
+		
+		Number result = (Number) query.getSingleResult();
+		
+		return result.longValue();
+	};
+	
+}
